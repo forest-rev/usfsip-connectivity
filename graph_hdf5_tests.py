c0870a47ee9ec339a339cb736863f4892b2a7ee0
Patrick Jantz <pj276@nau.edu>
2022-09-12 11:46:03 -0700

    first commit for hdf5 tests

---
+++ graph_hdf5_tests.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 09:54:51 2022
+
+@author: pj276
+"""
+import numpy as np, math, time
+from scipy import sparse
+from scipy.sparse import diags
+from pylab import imshow
+import networkx as nx
+import networkit as nk
+import rasterio as rio
+import h5py
+import scipy.sparse as ss
+import h5sparse
+from osgeo import gdal
+import pandas as pd
+import random
+
+#%%
+nk.getMaxNumberOfThreads() # see maximum number of available threads
+
+#%%
+# Functions
+# Minor modification from 
+# https://stackoverflow.com/questions/30199070/how-to-create-a-4-or-8-connected-adjacency-matrix
+# Diagonal weights set to sqrt(2). Orthogonal weights set to 1.
+# Calculation for conductance is then conductance value/cellres*weight
+def connected_adjacency(image, connect, patch_size=(1, 1)):
+    """
+    Creates an adjacency matrix from an image where nodes are considered adjacent 
+    based on 4-connected or 8-connected pixel neighborhoods.
+    :param image: 2 or 3 dim array
+    :param connect: string, either '4' or '8'
+    :param patch_size: tuple (n,m) used if the image will be decomposed into 
+                   contiguous, non-overlapping patches of size n x m. The 
+                   adjacency matrix will be formed from the smaller sized array
+                   e.g. original image size = 256 x 256, patch_size=(8, 8), 
+                   then the image under consideration is of size 32 x 32 and 
+                   the adjacency matrix will be of size 
+                   32**2 x 32**2 = 1024 x 1024
+    :return: adjacency matrix as a sparse matrix (type=scipy.sparse.csr.csr_matrix)
+    """
+    r, c = image.shape[:2]
+    r = int(r / patch_size[0])
+    c = int(c / patch_size[1])
+    if connect == '4':
+        # constructed from 2 diagonals above the main diagonal
+        d1 = np.tile(np.append(np.ones(c-1), [0]), r)[:-1]
+        d2 = np.ones(c*(r-1))
+        upper_diags = diags([d1, d2], [1, c])
+        return upper_diags + upper_diags.T
+    elif connect == '8':
+        # constructed from 4 diagonals above the main diagonal
+        d1 = np.tile(np.append(np.ones(c-1), [0]), r)[:-1]
+        d2 = np.append([0], d1[:c*(r-1)])
+        d3 = np.ones(c*(r-1))
+        d4 = d2[1:-1]
+        d4[d4==1] = 2.0**0.5
+        upper_diags = diags([d1, d2, d3, d4], [1, c-1, c, c+1])
+        return upper_diags + upper_diags.T
+    else:
+        raise ValueError('Invalid parameter \'connect\'={connect}, must be "4" or "8".'
+                     .format(connect=repr(connect)))
+
+# Get chunk ranges
+# From https://stackoverflow.com/questions/434287/what-is-the-most-pythonic-way-to-iterate-over-a-list-in-chunks
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+#%%
+# Read coordinates
+# Set your working directory
+dataDir = 'G:/My Drive/Projects/USFSIP_Connectivity/Data/GeoData/cleo_test/'
+outDir = 'G:/My Drive/Projects/USFSIP_Connectivity/Data/GeoData/cleo_test/out'
+
+# XYfile name
+xyfilename = dataDir + '/xys/SP_base.xy'
+
+# Xys
+xys = pd.read_csv(xyfilename)
+#atest = coord2pixelOffset(aa, xys.X, xys.Y)
+#sources = (np.array(atest[0])+1)*(np.array(atest[1])+1)-1
+
+infile = dataDir + '/resistance/resist_base_roads.rsg'
+# Use dataset index method to get cell numbers for each coordinate pair
+with rio.open(infile) as ds:
+    cinds = [ds.index(i[1].X,i[1].Y) for i in xys.iterrows()]
+    sources = [(i[0]*ds.shape[1])+i[1] for i in cinds]
+    A = ds.read(1)
+    A[A==-9999]=1000
+    tic = time.perf_counter()
+    adj8 = connected_adjacency(ds, '8').astype("float32").tocsr()
+    sinds = sparse.find(adj8)
+    A = A.flatten()
+    adj8[sinds[0],sinds[1]] = ((A[sinds[0]] + A[sinds[1]])/2)*sinds[2]*500
+    G8 = nx.from_scipy_sparse_array(adj8)
+    nx.set_node_attributes(G8, dict(zip(range(0,G8.number_of_nodes()), [{'value': i} for i in A])))
+    
+    node_values = nx.get_node_attributes(G8, 'value')
+    G8.remove_nodes_from((n for n, w in node_values.items() if w == 1000))
+    #edge_weights = nx.get_edge_attributes(G8,'weight')
+    #G8.remove_edges_from((e for e, w in edge_weights.items() if w == 0))
+    aa = nk.nxadapter.nx2nk(G8, weightAttr='weight')
+    aa.indexEdges()
+    toc = time.perf_counter()
+    print(f"The process took {toc - tic:0.4f} seconds")
+
+#%%
+import sys, os
+os.chdir('C:/data/')
+
+#%%
+# Write data in chunks to an array in the same dataset
+# Takes about 45s per chunk (291 chunks total when running 1000 nodes at a time)
+# Create a huge file because the array needs to be square
+
+# Timer
+tic0 = time.perf_counter()
+
+# Number of nodes from which to calculate distances for each iteration
+cLen = 500
+
+# hdf file to hold distances
+
+#f = h5py.File('cost_distances.h5', mode='a', rdcc_nbytes=1024**2*4000)
+# (10,16671)
+f = h5py.File('cost_distances.h5', mode='a')
+dset = f.create_dataset("dmat", (len(G8.nodes), len(G8.nodes)),
+                        compression="gzip", shuffle=True,
+                        chunks=True, dtype='uint16', compression_opts=9)
+#dset = f.create_dataset("dmat", (len(G8.nodes), len(G8.nodes)),
+#                        compression="lzf", shuffle=True,
+#                        chunks=True, dtype='f')
+f.close()
+
+#xx = list(chunker(list(range(len(list(G8.nodes)))),cLen))[80:82]
+#for i in xx:
+for i in chunker(list(range(len(list(G8.nodes)))),cLen):
+    tic = time.perf_counter()
+    spsp = nk.distance.SPSP(aa, i)
+    spsp.run()
+    dists = spsp.getDistances()
+    darr = np.array(dists)
+    darr = np.tril(darr, i[0]-1)
+    darr[darr==sys.float_info.max] = 0
+    darr[darr > 500000] = 0
+    darr = np.rint(darr/10).astype('uint16')
+    #print(darr)
+    with h5py.File('cost_distances.h5', 'a', ) as f:
+        f['dmat'][np.min(i):(np.max(i)+1),:] = darr
+    toc = time.perf_counter()
+    print(f"The subprocess took {toc - tic:0.4f} seconds")
+
+toc0 = time.perf_counter()
+print(f"The entire process took {toc0 - tic0:0.4f} seconds")
+
+# Access
+tic = time.perf_counter()
+f = h5py.File('cost_distances.h5', mode='r')
+f['dmat'][[0,5000,8000,10000,20000,30000,50000,100000,150000,200000],:]
+#f['dmat'][[0,1],:]
+f.close()
+toc = time.perf_counter()
+print(f"Accessing rows took {toc - tic:0.4f} seconds")
+
+# Loop access
+tic = time.perf_counter()
+elist = []
+with h5py.File('cost_distances.h5', 'r') as f:
+    for p in [0,5000,8000,10000,20000,30000,50000,100000,150000,200000]:
+        elist.append(f['dmat'][p,:])
+toc = time.perf_counter()
+print(f"Accessing rows took {toc - tic:0.4f} seconds")
+
+# Loop access
+tic = time.perf_counter()
+elist = []
+with h5py.File('cost_distances.h5', 'r') as f:
+    for p in [(0,20),(5000,6000),(8000,200000),(10000,19000),
+              (20000,40000),(30000,60000),(50000,120000),(100000,190000),
+              (150000,200050),(200000,200060)]:
+        elist.append(f['dmat'][p[1],p[0]])
+toc = time.perf_counter()
+print(f"Accessing rows took {toc - tic:0.4f} seconds")
+
+# Verify upper triangle is zeros
+tic = time.perf_counter()
+with h5py.File('cost_distances.h5', 'r') as f:
+    for p in [1000,91500]:
+        print(np.max(f['dmat'][p,p:-1]))
+        print(np.max(f['dmat'][p,:]))
+toc = time.perf_counter()
+print(f"Accessing rows took {toc - tic:0.4f} seconds")
+
+#%%
+#import h5sparse
+
+from os.path import exists
+
+# Write sparse matrices in chunks to an array in the same dataset
+# Takes about xxs per chunk (291 chunks total when running 1000 nodes at a time)
+
+# Timer
+tic0 = time.perf_counter()
+
+# Number of nodes from which to calculate distances for each iteration
+cLen = 1000
+
+# hdf file to hold distances
+
+#f = h5py.File('cost_distances.h5', mode='a', rdcc_nbytes=1024**2*4000)
+# (10,16671)
+
+#dset = f.create_dataset("dmat", (len(G8.nodes), len(G8.nodes)),
+#                        compression="lzf", shuffle=True,
+#                        chunks=True, dtype='f')
+
+
+#xx = list(chunker(list(range(len(list(G8.nodes)))),cLen))[80:82]
+#for i in xx:
+for i in chunker(list(range(len(list(G8.nodes)))),cLen):
+    tic = time.perf_counter()
+    spsp = nk.distance.SPSP(aa, i)
+    spsp.run()
+    dists = spsp.getDistances()
+    darr = np.array(dists)
+    darr = np.tril(darr, i[0]-1)
+    darr[darr==sys.float_info.max] = 0
+    darr[darr > 500000] = 0
+    darr = np.rint(darr/10).astype('uint16')
+    #print(darr)
+    if not exists('cost_distances_sp.h5'):
+        f = h5sparse.File('cost_distances_sp.h5', mode='a')
+        # Initialize with 
+        f.create_dataset("dmat", maxshape=(None,), data=ss.csr_matrix(darr),
+                                compression="gzip", shuffle=True,
+                                chunks=True, dtype='uint16', compression_opts=6)
+        f.close()
+    else:
+        with h5sparse.File('cost_distances_sp.h5', 'a', ) as f:
+            f['dmat'].append(ss.csr_matrix(darr))
+    toc = time.perf_counter()
+    print(f"The subprocess took {toc - tic:0.4f} seconds")
+
+toc0 = time.perf_counter()
+print(f"The entire process took {toc0 - tic0:0.4f} seconds")
+
+# Loop access
+tic = time.perf_counter()
+elist = []
+with h5sparse.File('cost_distances_sp.h5', 'r') as f:
+    for p in [0,5000,8000,10000,20000,30000,50000,100000,150000,200000]:
+        elist.append(f['dmat'][p:p+1])
+toc = time.perf_counter()
+print(f"Accessing rows took {toc - tic:0.4f} seconds")
+
+
+# Loop access
+tic = time.perf_counter()
+elist = []
+with h5sparse.File('cost_distances_sp.h5', 'r') as f:
+    for p in [(0,20),(5000,6000),(8000,200000),(10000,19000),
+              (20000,40000),(30000,60000),(50000,120000),(100000,190000),
+              (150000,200050),(200000,200060)]:
+        elist.append(f['dmat'][p[1]:p[1]+1].toarray()[0,p[0]])
+toc = time.perf_counter()
+print(f"Accessing rows took {toc - tic:0.4f} seconds")
+
+#%%
+# Write each column as a separate dataset
+# Timer
+tic0 = time.perf_counter()
+
+# Number of nodes from which to calculate distances for each iteration
+cLen = 1000
+
+# hdf file to hold distances
+
+f = h5py.File('cost_distances.h5', mode='a')
+#dset = f.create_dataset("dmat", (len(G8.nodes), len(G8.nodes)), compression="gzip", shuffle=True, chunks=True, dtype='f')
+f.close()
+
+xx = list(chunker(list(range(len(list(G8.nodes)))),cLen))[150:152]
+for i in xx:
+#for i in chunker(list(range(len(list(G8.nodes)))),cLen):
+    tic = time.perf_counter()
+    spsp = nk.distance.SPSP(aa, i)
+    spsp.run()
+    dists = spsp.getDistances()
+    with h5py.File('cost_distances.h5', 'a') as f:
+        # j is relative to 0, k is relative to chunk indices
+        for j,k in enumerate(i):
+            dset = f.create_dataset(str(k), len(dists[j][k:len(dists[j])]), compression="gzip", shuffle=True, chunks=True, dtype='f')
+            dset[:] = dists[j][k:len(dists[j])]
+    toc = time.perf_counter()
+    print(f"The subprocess took {toc - tic:0.4f} seconds")
+
+toc0 = time.perf_counter()
+print(f"The entire process took {toc0 - tic0:0.4f} seconds")
